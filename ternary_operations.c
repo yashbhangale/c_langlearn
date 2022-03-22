7b4fb94d90a012a691ac8e0000cfdb28dffa6113
yashbhangale <68957369+yashbhangale@users.noreply.github.com>
2022-03-22 22:07:49 +0530

    Add files via upload

---
+++ ternary_operations.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter a\n");
+    scanf("%d", &a);
+
+    (a < 5) ? printf("A i less than 5") : printf("A is notless than 5");
+    return 0;
+}
