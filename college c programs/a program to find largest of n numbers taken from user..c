aab8aaa1512f96de3e46ea81df9a2d76690c5b26
yashbhangale <68957369+yashbhangale@users.noreply.github.com>
2022-04-05 23:06:43 +0530

    Create a program to find largest of n numbers taken from user..c

---
+++ college c programs/a program to find largest of n numbers taken from user..c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main( )
+{
+	int n,i,a[100],large;
+	clrscr();
+     	printf("Enter the number of elements:");
+	scanf("%d",&n);
+	for(i=0;i<=n-1;i++) 
+  {
+    printf("Enter a value:");    scanf("%d",&a[i]); 
+    
+  }
+  
+  
+  
+	large=a[0];
+	for(i=1;i<=n-1;i++) { 	if(large<a[i])  large=a[i]; }
+	printf("The largest number is %d",large);
+	getch();
+}
